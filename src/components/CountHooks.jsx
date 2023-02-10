e2190b8edddd5eab89fdea7e12c3a022d905ba91
Beatriz Recari <beatrizrecari@gmail.com>
2023-02-10 10:08:51 -0500

    created both class and functional components

---
+++ src/components/CountHooks.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+export default function CountHooks() {
+    const [count, setCount] = useState(0)
+    const [title, setTitle] = useState('Welcome')
+    useEffect(() => {
+        setTitle('Our New Title')
+    }, [])
+    return(
+        <section>
+                <h2>{title}</h2>
+                <h3>You Clicked {count} times.</h3>
+                <button onClick={ () => setCount(count + 1)}>
+                    Click Me</button>
+            </section>
+    )
+}
